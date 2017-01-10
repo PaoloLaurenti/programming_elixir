c01b46afcc8ca7ed8c0bb7af0b94c211e906c465
Paolo Laurenti <laurentipaolo@gmail.com>
2017-01-10 13:54:09 +0100

    Complete ModulesAndFunctions-7 exercise

--- chapter6/modules_and_functions.exs
+++ chapter6/modules_and_functions.exs
@@ -14,4 +14,22 @@ defmodule ModulesAndFunctions do
   def gcd(x, y) do
     gcd(y, rem(x, y))
   end
+
+  def convert_float_to_string(value) do
+    :io_lib.format("~.2f", [value])
+      |> List.flatten
+  end
+
+  def get_environment_variable(variable_name) do
+    System.get_env(variable_name)
+  end
+
+  def get_file_extension(file_path) do
+    Path.extname(file_path)
+  end
+
+  def get_current_woring_directory() do
+    System.cwd()
+  end
+
 end
