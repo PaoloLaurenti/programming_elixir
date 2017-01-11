388a360596f6e0f86bc39da796e043f3bb5982e3
Paolo Laurenti <laurentipaolo@gmail.com>
2017-01-11 13:41:07 +0100

    Complete ListsAndRecursion-1 exercise

--- chapter7/lists_and_recursion.exs
+++ chapter7/lists_and_recursion.exs
@@ -7,4 +7,9 @@ defmodule ListsAndRecursion do
 
   def sum_no_acc([]), do: 0
   def sum_no_acc([head | tail]), do: head + sum_no_acc(tail)
+
+  def mapsum(list, function), do: _mapsum(list, function, 0)
+
+  defp _mapsum([], _, total), do: total
+  defp _mapsum([head | tail], function, total), do: _mapsum(tail, function, total + function.(head))
 end
