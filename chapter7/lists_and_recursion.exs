a0568b51c75043b501ad04663a36e7c1ff683330
Paolo Laurenti <laurentipaolo@gmail.com>
2017-01-12 08:57:13 +0100

    Complete ListsAndRecursion-4 exercise

--- chapter7/lists_and_recursion.exs
+++ chapter7/lists_and_recursion.exs
@@ -1,5 +1,9 @@
 defmodule ListsAndRecursion do
 
+  #############################################################################
+  # Exercise: ListsAndRecursion-0
+  # I defined our sum function to carry a partial total as a second parameter so I could illustrate how to use accumulators to build values.
+  # The sum function can also be written without an accumulator. Can you do it?
   def sum(list), do: _sum(list, 0)
 
   defp _sum([], total), do: total
@@ -7,12 +11,20 @@ defmodule ListsAndRecursion do
 
   def sum_no_acc([]), do: 0
   def sum_no_acc([head | tail]), do: head + sum_no_acc(tail)
+  #############################################################################
 
+  #############################################################################
+  # Exercise: ListsAndRecursion-1
+  # Write a mapsum function that takes a list and a function. It applies the function to each element of the list and then sums the result
   def mapsum(list, function), do: _mapsum(list, function, 0)
 
   defp _mapsum([], _, total), do: total
   defp _mapsum([head | tail], function, total), do: _mapsum(tail, function, total + function.(head))
+  #############################################################################
 
+  #############################################################################
+  # Exercise: ListsAndRecursion-2
+  # Write a max(list) that returns the element with the maximum value in the list
   def max([head | tail] = list), do: _max(list, head)
 
   defp _max([], max_result), do: max_result
@@ -23,14 +35,31 @@ defmodule ListsAndRecursion do
       _max(tail, head)
     end
   end
+  #############################################################################
 
+  #############################################################################
+  # Exercise: ListsAndRecursion-3
+  # An Elixir single-quoted string is actually a list of individual character codes.
+  # Write a caesar(list, n) function that adds n to each list element, wrapping if the addition results in a character greater than z
   def caesar(list, n), do: _caesar(list, n, [])
 
-  defp _caesar([], _, result), do: Enum.reverse(result) 
+  defp _caesar([], _, result), do: Enum.reverse(result)
   defp _caesar([head | tail], n, result) when head + n <= ?z do
     _caesar(tail, n, [head + n | result])
   end
   defp _caesar([head | tail], n, result) do
     _caesar(tail, n, [head + n - 26 | result])
   end
+  #############################################################################
+
+  #############################################################################
+  # Exercise: ListsAndRecursion-4
+  # Write a function MyList.span(from, to) that returns a list of the numbers from from up to to
+  def span(from, to) when from > to, do: raise "From parameter must be smaller than to parameter"
+  def span(from, to), do: _span(from, to, [])
+
+  defp _span(to, to, result), do: [to | result] |> Enum.reverse
+  defp _span(from, to, partial_result), do: _span(from + 1, to, [from | partial_result])
+  #############################################################################
+
 end
