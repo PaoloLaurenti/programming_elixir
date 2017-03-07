9451efb1dc7b90ecf8553d4f4150fa4a0aee8dd8
Paolo Laurenti <laurentipaolo@gmail.com>
2017-03-07 14:16:14 +0100

    Complete StringAndBinaries-2 exercise: write a function that says if two single quoted strings are anagrams

--- chapter11/strings_and_binaries.exs
+++ chapter11/strings_and_binaries.exs
@@ -2,11 +2,24 @@ defmodule StringsAndBinaries do
 
   #############################################################################
   # Exercise: StringsAndBinaries-1
-  # Write a function that returns true if single-quoted string contains only printable ASCII characters (space through tilde).
+  # Write a function that returns true if single-quoted string contains only
+  # printable ASCII characters (space through tilde).
   ############################################################################
 
-  def is_printable(string) when is_list(string) do
+  def is_printable?(string) when is_list(string) do
     Enum.all?(string, &(&1 >= 32 && &1 <= 126))
+  end
+
+  ############################################################################
+
+  ############################################################################
+  # Exercise: StringsAndBinaries-2
+  # Write an anagram?(word1, word2) that returns true if its parameters are
+  # anagrams.
+  ############################################################################
+
+  def anagram?(word1, word2) do
+    Enum.count(word1) === Enum.count(word2) and not Enum.any?(word1, fn(character) -> not Enum.any?(word2, &(&1 == character)) end)
   end
 
   ############################################################################
