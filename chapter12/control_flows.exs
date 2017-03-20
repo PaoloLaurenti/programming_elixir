43ce949dfaf4a76340006c8df590565c811cd9e7
Paolo Laurenti <laurentipaolo@gmail.com>
2017-03-20 08:52:53 +0100

    Complete ControlFlows-3 exercise: Write an ok! function that takes an arbitrary parameter. If the parameter is the tuple {:ok, data} , return the data. Otherwise, raise an exception containing information from the parameter

--- chapter12/control_flows.exs
+++ chapter12/control_flows.exs
@@ -20,4 +20,20 @@ defmodule ControlFlows do
 
   #############################################################################
 
+  #############################################################################
+  # Exercise: ControlFlow-2
+  # Many built-in functions have two forms. The xxx form returns the tuple
+  # {:ok, data} and the xxx! form returns data on success but raises an exception
+  # otherwise. However, some functions don’t have the xxx! form. Write an ok!
+  # function that takes an arbitrary parameter. If the parameter is the tuple
+  # {:ok, data} , return the data. Otherwise, raise an exception containing
+  # information from the parameter.
+  #############################################################################
+
+  def ok!({:ok, data}), do: data
+  def ok!({:error, message}), do: raise to_string(message)
+  def ok!(_), do: raise "unknown parameter"
+
+  #############################################################################
+
 end
