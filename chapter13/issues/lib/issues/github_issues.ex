87a0f4396e643f50a3f0cbfb774572cf6698e16a
Paolo Laurenti <laurentipaolo@gmail.com>
2017-04-02 18:50:37 +0200

    Put GItHub url in configuration of Issue application of chapter 13

--- chapter13/issues/lib/issues/github_issues.ex
+++ chapter13/issues/lib/issues/github_issues.ex
@@ -9,6 +9,9 @@
 defmodule Issues.GithubIssues do
   @user_agent  [ {"User-agent", "Elixir dave@pragprog.com"} ]
 
+  # use a module attribute to fetch the value at compile time
+  @github_url Application.get_env(:issues, :github_url)
+
   def fetch(user, project) do
     issues_url(user, project)
     |> HTTPoison.get(@user_agent)
@@ -16,7 +19,7 @@ defmodule Issues.GithubIssues do
   end
 
   def issues_url(user, project) do
-    "https://api.github.com/repos/#{user}/#{project}/issues"
+    "#{@github_url}/repo/#{user}/#{project}/issues"
   end
 
   def handle_response({ :ok, %{status_code: 200, body: body}}) do
