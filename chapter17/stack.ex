937d49e6d2b56803dd3230a483cf4656cb3be99e
Paolo Laurenti <laurentipaolo@gmail.com>
2017-04-29 19:59:41 +0200

    Complete "OTP-Servers-4" exercise of chapter 17

--- chapter17/stack.ex
+++ chapter17/stack.ex
@@ -1,7 +1,19 @@
 defmodule Stack do
   use GenServer
 
-  def handle_call({:pop}, _from, [head | stack]) do
+  def start_link(list) do
+    GenServer.start_link(__MODULE__, list, name: __MODULE__)
+  end
+
+  def pop() do
+    GenServer.call __MODULE__, :pop
+  end
+
+  def push(value) do
+    GenServer.cast __MODULE__, {:push, value}
+  end
+
+  def handle_call(:pop, _from, [head | stack]) do
     {:reply, head, stack}
   end
 
