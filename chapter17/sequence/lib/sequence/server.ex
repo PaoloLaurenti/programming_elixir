8755c79a6fda01b1b970080dde6534642f73916b
Paolo Laurenti <laurentipaolo@gmail.com>
2017-04-29 19:53:02 +0200

    Tidy the interface of sequence server of chapter 17

--- chapter17/sequence/lib/sequence/server.ex
+++ chapter17/sequence/lib/sequence/server.ex
@@ -9,6 +9,24 @@
 defmodule Sequence.Server do
   use GenServer
 
+  #####
+  # External API
+
+  def start_link(current_number) do
+    GenServer.start_link(__MODULE__, current_number, name: __MODULE__)
+  end
+
+  def next_number do
+    GenServer.call __MODULE__, :next_number
+  end
+
+  def increment_number(delta) do
+    GenServer.cast __MODULE__, {:increment_number, delta}
+  end
+
+  #####
+  # GenServer implementation
+
   def handle_call(:next_number, _from, current_number) do
     { :reply, current_number, current_number+1 }
   end
@@ -20,5 +38,27 @@ defmodule Sequence.Server do
   def format_status(_reason, [ _pdict, state ]) do
     [data: [{'State', "My current state is '#{inspect state}', and I'm happy"}]]
   end
-
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
